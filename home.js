e91ce2a1c03c58028067cf3adfb4ab4550b3435d
TMerco <tyrellcode@gmail.com>
2022-10-14 14:17:46 -0700

    Objects and DOM

--- home.js
+++ home.js
@@ -116,19 +116,148 @@
 
 //FUNCTION RETURN VALUES \\
 
-function getSecretCode(value) {
-  let code = value * 42;
-  return code;
-}
+// function getSecretCode(value) {
+//   let code = value * 42;
+//   return code;
+// }
 
-showMessage(getSecretCode(2));  //2 replaces 'value' which makes the function 
-console.log(getSecretCode(2)); //"2 * 42" instead of "value * 42"
+// showMessage(getSecretCode(2));  //2 replaces 'value' which makes the function 
+// console.log(getSecretCode(2)); //"2 * 42" instead of "value * 42"
 
-//A Cleaner Way to Present Same Code Above
-function getSecretCode(value) {
-  let code = value * 42;
-  return code;
-}
+// //A Cleaner Way to Present Same Code Above
+// function getSecretCode(value) {
+//   let code = value * 42;
+//   return code;
+// }
 
-let secretCode = getSecretCode(2);  //Using very clear variable names makes
-showMessage(secretCode);            //The code cleaner.
+// let secretCode = getSecretCode(2);  //Using very clear variable names makes
+// showMessage(secretCode);            //The code cleaner.
+
+
+
+
+
+// //FUNCTION SCOPE \\
+// //Functions can use code from outside as long as it has 
+// //a variable, but not the other way around.
+
+// function getSecretCode(value){
+//   let code = value * 42;
+// }
+
+// let secretCode = getSecretCode(2);
+// showMessage(code);
+
+//Example: 
+
+// let key = 42;
+
+// function getSecretCode(value){
+  
+//   let keyGenerator = function() {
+//     let key = 12;
+//     console.log('in keyGenerator: ', key);
+//     return key;
+//   }
+
+//   let code = value * keyGenerator();
+//   console.log(' in keyGenerator: ', key);
+//   return code;
+// }
+
+// let secretCode = getSecretCode(2);
+// showMessage(secretCode);
+
+
+
+
+//OBJECT PROPERTIES \\
+// //Object is a group of values/properties.
+// let mySymbol = symbol();
+
+// let person = {         //Here is the Object
+//   name: "John",      // Here are Properties
+//   age: 32,            // Here are Properties
+//   partTime: false,      // Here are Properties
+//   [mySymbol]: 'secretInformation'
+// };
+
+// person.age = 33;  //change an element of a property inside the object
+// person['age'] = 44; //another way to change it
+// //How to access Object Properties.
+// showMessage(person.age);
+
+
+
+
+//Object Methods \\
+
+// let person = {
+//   name: 'John',
+//   age: 32,
+//   partTime: false,
+//   showInfo: function(realAge) {     //"this." refers to the current object
+//     showMessage(this.name + ' is ' + realAge); //this.propertyName referes to the property 
+// }                                               // name inside the current object
+// }
+
+// person.showInfo(34);
+
+
+
+// //PASSING OBJECTS TO FUNCTIONS \\
+
+// let message = 'Hello';
+
+// function changeMessage(message) {
+//   message = 'Hi!';
+// }
+
+// changeMessage(message);
+// showMessage(message);
+//Result is message because the change to 'Hi!' is stored into the parameter of the function
+//instead of changing the value of message when calling:  showMessage(message);
+
+// //HOW IT WORKS WITH OBJECTS \\
+
+// let person = {
+//   name: 'John',
+//   age: 32,
+//   partTime: false
+// };
+
+// function incrementAge(person) {
+//   person.age++;
+// }
+
+// incrementAge(person);
+// // showMessage(person.age);
+
+
+// //STANDARD BUILT-IN OBJECTS \\
+// /*
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects
+// */
+
+// //DATE 
+// let now = new Date();
+//  showMessage(now.toDateString());
+
+//  //MATH
+//  showMessage(Math.random(-42));
+
+//  //STRING
+
+//  let s = 'Hello';
+//  showMessage(s.charAt(0));
+
+
+ 
+
+
+
+
+
+ //DOM (Document Object Model) \\
+
+ showMessage ();
